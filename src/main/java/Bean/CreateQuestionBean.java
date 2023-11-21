d997a3524a42a689efb9a8321ff52487a2f373ed
AsierContreras <asicontre@gmail.com>
2023-11-21 13:02:31 +0100

    JSF inicializado

---
+++ src/main/java/Bean/CreateQuestionBean.java
@@ -0,0 +1,46 @@
+package Bean;
+
+import java.util.Date;
+
+import domain.Event;
+import domain.Question;
+
+public class CreateQuestionBean {
+	private Date fecha;
+	private Event partido;
+	private Question pregunta;
+	private int minBet;
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+
+	public Event getPartido() {
+		return partido;
+	}
+
+	public void setPartido(Event partido) {
+		this.partido = partido;
+	}
+
+	public Question getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(Question pregunta) {
+		this.pregunta = pregunta;
+	}
+
+	public int getMinBet() {
+		return minBet;
+	}
+
+	public void setMinBet(int minBet) {
+		this.minBet = minBet;
+	}
+
+}
